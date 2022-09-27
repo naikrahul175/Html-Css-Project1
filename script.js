b1e139e805b95f13c13fff2787ea310b216502c0
naikrahul175 <naikrahul175@gmail.com>
2022-09-27 06:13:09 +0530

    added eventListner for buttons

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
     
 const crypto = [
     {id:1, Assets:"Bitcon",  UpdatePrice:602740.4, Changes:-1.46, logo: "./images/BITCOIN.png" },
-    {id:2, Assets:"ETHereum",  UpdatePrice:3496, Changes:-0.02,  logo: "./images/BITCOIN.png"},
-    {id:3, Assets:"Binance coin",  UpdatePrice:389.29, Changes: +1.16 ,  logo: "./images/BITCOIN.png"},
-    { id:4, Assets: 'LTC litecoin',  UpdatePrice:199.16, Changes:-0.11,  logo: "./images/BITCOIN.png"}
+    {id:2, Assets:"ETHereum",  UpdatePrice:3496, Changes:-0.02,  logo: "./images/ETHEREM.png"},
+    {id:3, Assets:"Binance coin",  UpdatePrice:389.29, Changes: +1.16 ,  logo: "./images/BINANCECOIN.png"},
+    { id:4, Assets: 'LTC litecoin',  UpdatePrice:199.16, Changes:-0.11,  logo: "./images/LTC.png"}
   ]
 
   console.log(crypto);
@@ -24,10 +24,19 @@ function loadTableData() {
     document.getElementById("table-body").innerHTML = tableData;
 }
 
-function closebtn() {
+function closebtn(id) {
     //   let closebtn = document.getElementById("ad-close");
-    //   console.log(closebtn);
-    document.getElementById("ads-container").style.display = "none";
+      console.log(id);
+    // document.getElementById("ads-container").style.display = "none";
+    // document.getElementById("ads-col-1").style.display = "none";
+    // document.getElementById("ads-col-2").style.display = "none";
+    document.getElementById(id).style.display = "none";
+    document.getElementById("banner-container").style.top = 0;
+}
+function viewMore() {
+
+    alert("Pending work")
+    // document.getElementById(id).alert("Pending work");
 }
 
 loadTableData()
